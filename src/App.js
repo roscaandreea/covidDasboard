f434346fc9aeb5132841e46b68ba065ba5d91ead
roscaandreea <andreea.rosca@info.uaic.ro>
2021-08-26 15:44:52 +0300

    Starting to add title and input dropdown field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
+import covidImage from './images/image.png';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a Covid Dasboard</h1>
+      <img className="image" src={covidImage} alt="covid-19" />
+      <h1 className="text">current status</h1>
+      <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc" 
+          >
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">YOOOOO</MenuItem>
+          </Select>
+      </FormControl>
+      {/* Header*/}
+      {/* Title + select input dropdown field */}
+
+
+       {/* InfoBoxs */}
+       {/* InfoBoxs */}
+       {/* InfoBoxs */}
+       
+       {/* Table */}
+       {/* Graph */}
+
+       {/* Map */}
+
     </div>
   );
 }
