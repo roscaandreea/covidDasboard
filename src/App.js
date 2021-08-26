a24c5fa106e82dee9954094438be3b715ed66d42
roscaandreea <andreea.rosca@info.uaic.ro>
2021-08-27 00:07:40 +0300

    Divide entire app in two containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
+import Cards from './Cards';
+import Map from './Map';
 import covidImage from './images/image.png';
 import React, {useEffect, useState} from 'react';
 
 function App() {
   const [countries,setCountries] = useState([]);
+  const [country,setCountry] = useState(['global']);
   // https://disease.sh/v3/covid-19/countries
   useEffect(() =>{
     //code inside here will run once when the component is loads and not again
@@ -23,35 +26,42 @@ function App() {
     };
     getCountriesData();
   },[]);
+
+  const onCountryChange = (event) =>{
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
   return (
     <div className="app">
-      {/* Header*/}
-      {/* Title + select input dropdown field */}
-      <div className="app__header">
-        <img className="image" src={covidImage} alt="covid-19" />
-        <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            value="abc" 
-          >
-            {
-              countries.map(country =>(
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-          </Select>
-       </FormControl>
-     </div>
-     <h1 className="text">current status</h1>
-       {/* InfoBoxs */}
-       {/* InfoBoxs */}
-       {/* InfoBoxs */}
-       
-       {/* Table */}
-       {/* Graph */}
-
-       {/* Map */}
-
+      <div className="app__left">
+          {/* Header*/}
+          {/* Title + select input dropdown field */}
+          <div className="app__header">
+             <img className="image" src={covidImage} alt="covid-19" />
+             <FormControl className="app__dropdown">
+                <Select variant="outlined" value={country} onChange={onCountryChange}>
+                   <MenuItem value="global">Global</MenuItem>
+                      {
+                        countries.map(country =>(
+                            <MenuItem value={country.value}>{country.name}</MenuItem>
+                     ))}
+                </Select>
+            </FormControl>
+          </div>
+          <h1 className="text">current status</h1>
+              {/* InfoBoxs */}
+            <div className="app__stats">
+              <Cards title="Coronavirus cases:" cases={123} total={2000} />
+              <Cards title="Recovered:" cases={1235} total={3000} />
+              <Cards title="Deaths:" cases={1234} total={1000}/>
+            </div>
+            {/* Map */}
+          <Map />
+      </div>
+      <div className="app__right">
+        {/* Table */}
+        {/* Graph */}
+      </div>
     </div>
   );
 }
