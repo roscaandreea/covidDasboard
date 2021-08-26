e592e8dac804336706712e06876525c58aafecee
roscaandreea <andreea.rosca@info.uaic.ro>
2021-08-26 16:06:38 +0300

    Just added a container for title and input dropdown plus some styles

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import covidImage from './images/image.png';
 function App() {
   return (
     <div className="app">
-      <img className="image" src={covidImage} alt="covid-19" />
-      <h1 className="text">current status</h1>
-      <FormControl className="app__dropdown">
+      {/* Header*/}
+      {/* Title + select input dropdown field */}
+      <div className="app__header">
+        <img className="image" src={covidImage} alt="covid-19" />
+        <FormControl className="app__dropdown">
           <Select
             variant="outlined"
             value="abc" 
           >
+            {/* Loop through all the countries and show a
+            dropdown list of the options */}
+            { /*
             <MenuItem value="worldwide">WorldWide</MenuItem>
             <MenuItem value="worldwide">Option 2</MenuItem>
             <MenuItem value="worldwide">Option 3</MenuItem>
-            <MenuItem value="worldwide">YOOOOO</MenuItem>
+            <MenuItem value="worldwide">YOOOOO</MenuItem> */}
           </Select>
-      </FormControl>
-      {/* Header*/}
-      {/* Title + select input dropdown field */}
-
-
+       </FormControl>
+     </div>
+     <h1 className="text">current status</h1>
        {/* InfoBoxs */}
        {/* InfoBoxs */}
        {/* InfoBoxs */}
